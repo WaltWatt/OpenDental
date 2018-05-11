5d4210f51701cd308a4440915b28f3da7b32cac4
andrewd <andrewd@098d0599-7f2b-0410-95a5-ad30ac85393a>
2018-05-11 19:44:01 +0000

    E7063 - Patient Portion Uncollected Report executes very slowly on large databases when run for multiple month date ranges. Backported to: 17.4.66.0;18.1.7.0 Reviewed by: Joe
    git-svn-id: https://opendentalsoft.com:23793/svn/opendental/opendental18.1@19578 098d0599-7f2b-0410-95a5-ad30ac85393a

--- OpenDentBusiness/Reports/RpPatPortionUncollected.cs
+++ OpenDentBusiness/Reports/RpPatPortionUncollected.cs
@@ -6,6 +6,7 @@ using System.Data;
 using System.Linq;
 using System.Reflection;
 using System.Text;
+using System.Diagnostics;
 
 namespace OpenDentBusiness {
 	public class RpPatPortionUncollected {
@@ -14,6 +15,10 @@ namespace OpenDentBusiness {
 			if(RemotingClient.RemotingRole==RemotingRole.ClientWeb) {
 				return Meth.GetTable(MethodBase.GetCurrentMethod(),dateFrom,dateTo,listClinicNums);
 			}
+			#if DEBUG
+			Stopwatch s=new Stopwatch();
+			s.Start();
+			#endif
 			bool hasClinicsEnabled=ReportsComplex.RunFuncOnReportServer(() =>(!Prefs.GetBoolNoCache(PrefName.EasyNoClinics)));
 			string query="SELECT procedurelog.ProcDate,CONCAT(patient.LName,', ',patient.FName)AS Patient, procedurecode.AbbrDesc, "
 				+"procedurelog.ProcFee*(procedurelog.BaseUnits+procedurelog.UnitQty)-IFNULL(cp.CapWriteOff,0) Fee,"
@@ -30,7 +35,9 @@ namespace OpenDentBusiness {
 					+"SELECT SUM(adjustment.AdjAmt) AdjAmt, adjustment.ProcNum "
 					+"FROM adjustment "
 					+"WHERE adjustment.ProcNum != 0 "
+					+"AND adjustment.ProcDate BETWEEN "+POut.Date(dateFrom)+" AND "+POut.Date(dateTo)+" "
 					+"GROUP BY ProcNum "
+					+"ORDER BY NULL "
 				+")adj ON adj.ProcNum=procedurelog.ProcNum "
 				+"LEFT JOIN ( "
 					+"SELECT claimproc.Status, SUM(CASE WHEN claimproc.Status = "+POut.Int((int)ClaimProcStatus.NotReceived)+" THEN claimproc.InsPayEst "
@@ -45,22 +52,37 @@ namespace OpenDentBusiness {
 						+POut.Int((int)ClaimProcStatus.Received)+","
 						+POut.Int((int)ClaimProcStatus.Supplemental)+","
 						+POut.Int((int)ClaimProcStatus.CapComplete)+") "
+					+"AND claimproc.ProcDate BETWEEN "+POut.Date(dateFrom)+" AND "+POut.Date(dateTo)+" "
 					+"GROUP BY claimproc.ProcNum "
+					+"ORDER BY NULL "
 				+")cp ON cp.ProcNum=procedurelog.ProcNum "
 				+"LEFT JOIN ( "
 					+"SELECT SUM(paysplit.SplitAmt) SplitAmt, paysplit.ProcNum "
 					+"FROM paysplit "
+					+"INNER JOIN procedurelog "
+						+"ON paysplit.ProcNum=procedurelog.ProcNum "
+						+"AND procedurelog.ProcStatus=2 "//Complete
+						+"AND procedurelog.ProcDate BETWEEN "+POut.Date(dateFrom)+" AND "+POut.Date(dateTo)+" "
 					+"WHERE paysplit.ProcNum != 0 "
 					+"GROUP BY paysplit.ProcNum "
+					+"ORDER BY NULL "
 				+")pay ON pay.ProcNum=procedurelog.ProcNum "
 				+"WHERE procedurelog.ProcStatus=2 "
-				+"AND procedurelog.ProcDate BETWEEN "+POut.Date(dateFrom)+" AND  "+POut.Date(dateTo)+" ";
+				+"AND procedurelog.ProcDate BETWEEN "+POut.Date(dateFrom)+" AND  "+POut.Date(dateTo)+" "
+				+"AND IFNULL((procedurelog.ProcFee*(procedurelog.BaseUnits+procedurelog.UnitQty))-IFNULL(cp.CapWriteOff,0)-IFNULL(cp.InsAmt,0)-"
+					+"IFNULL(cp.WriteOff,0),0)+IFNULL(adj.AdjAmt,0)-IFNULL(pay.SplitAmt,0) > 0.005";
 				if(hasClinicsEnabled && listClinicNums.Count>0) {
 					query+="AND procedurelog.ClinicNum IN("+string.Join(",",listClinicNums.Select(x => POut.Long(x)))+") ";
 				}
-				query+="HAVING Uncollected > 0.005 "
-				+"ORDER BY procedurelog.ProcDate,patient.LName,patient.FName,procedurecode.ProcCode";
-			return ReportsComplex.RunFuncOnReportServer(() => Db.GetTable(query));
+				query+="ORDER BY procedurelog.ProcDate,patient.LName,patient.FName,procedurecode.ProcCode";
+			DataTable table=ReportsComplex.RunFuncOnReportServer(() => Db.GetTable(query));
+			#if DEBUG
+			s.Stop();
+			Console.WriteLine("Total time to generate report with "+string.Format("{0:#,##0.##}",table.Rows.Count)+" rows: "
+				+(s.Elapsed.Hours>0?(s.Elapsed.Hours+" hours "):"")+(s.Elapsed.Minutes>0?(s.Elapsed.Minutes+" min "):"")
+				+(s.Elapsed.TotalSeconds-(s.Elapsed.Hours*60*60)-(s.Elapsed.Minutes*60))+" sec");
+			#endif
+			return table;
 		}
 	}
 }
