94b57bfd2904469d71156ee142322b6643c38629
saul <saul@098d0599-7f2b-0410-95a5-ad30ac85393a>
2018-05-31 00:28:00 +0000

    version 18.1.17
    git-svn-id: https://opendentalsoft.com:23793/svn/opendental/opendental18.1@19774 098d0599-7f2b-0410-95a5-ad30ac85393a

--- CodeBase/Properties/AssemblyInfo.cs
+++ CodeBase/Properties/AssemblyInfo.cs
@@ -1,4 +1,4 @@
-﻿using System.Reflection;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("17.3.1.0")]
+[assembly: AssemblyVersion("18.1.17.0")]
 //[assembly: AssemblyFileVersion("1.0.0.0")]
