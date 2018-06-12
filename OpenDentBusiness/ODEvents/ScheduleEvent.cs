39fe91580f00265485e54115427e521dabefe103
jsalmon <jsalmon@098d0599-7f2b-0410-95a5-ad30ac85393a>
2018-06-12 00:11:26 +0000

    ** Missing file from previous commit. B7881 - (Save Commit) Schedules, loading the Schedule window takes a long time for large databases. Committed to: Head Only (18.2) Reviewed by: Derek
    git-svn-id: https://opendentalsoft.com:23793/svn/opendental/opendental18.1@19867 098d0599-7f2b-0410-95a5-ad30ac85393a

---
+++ OpenDentBusiness/ODEvents/ScheduleEvent.cs
@@ -0,0 +1,21 @@
+﻿using CodeBase;
+using System;
+
+namespace OpenDentBusiness {
+	///<summary></summary>
+	public class ScheduleEvent:IODEvent {
+		///<summary></summary>
+		public static event ODEventHandler Fired;
+
+		///<summary></summary>
+		public static void Fire(ODEventArgs e) {
+			if(Fired!=null) {
+				Fired(e);
+			}
+		}
+
+		public void FireEvent(ODEventArgs e) {
+			Fire(e);
+		}
+	}
+}
