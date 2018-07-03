d8cfb6ef20251d8d530a9c4ab5cb852d19bca5b1
jsalmon <jsalmon@098d0599-7f2b-0410-95a5-ad30ac85393a>
2018-07-03 20:55:34 +0000

    version 18.1.32
    git-svn-id: https://opendentalsoft.com:23793/svn/opendental/opendental18.1@20055 098d0599-7f2b-0410-95a5-ad30ac85393a

--- CodeBase/Properties/AssemblyInfo.cs
+++ CodeBase/Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("18.1.31.0")]
+[assembly: AssemblyVersion("18.1.32.0")]
 //[assembly: AssemblyFileVersion("1.0.0.0")]
